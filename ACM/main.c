51ae843adfe66e444005bfebcd291383441829f7
Nazia Afroz <naziaafroz@salehs-mac-mini.local>
2016-07-28 21:39:40 +0600

    Initial Commit

---
+++ ACM/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ACM
+//
+//  Created by Nazia Afroz on 7/28/16.
+//  Copyright © 2016 Nazia Afroz. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
